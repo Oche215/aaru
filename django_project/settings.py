d54d9428024eca8dbeed28e2a0ceae9716939497
OCHE Samson <s.oche@outlook.com>
2026-03-02 01:21:36 +0100

    Alloed Host for Railway added1

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-^19%8(99$@zvb^p=^rpq6$l!0)vk6q5h(ic0ngg^f$1c0w*pqh
 DEBUG = True
 
 ALLOWED_HOSTS = ['aaru-production.up.railway.app', 'https://aaru-production.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['aaru-production.up.railway.app', 'https://aaru-production.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://aaru-production.up.railway.app', 'https://127.0.0.1']
 
 
 # Application definition
