76624444fd56693e5ff55ca724f8451f191f7795
OCHE Samson <s.oche@outlook.com>
2026-03-04 15:49:22 +0100

    accounts profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,8 @@
 from django.urls import path
-from .views import (RegistrationView, AddProductView, product_table, accounts,
-                    ListProductsView, UpdateProductView, product_list, product_detail, edit_user, StaffListView)
+from .views import (accounts)
 
 
 urlpatterns = [
-    path('', accounts, name='accounts'  ),
-
-    path('register/', RegistrationView.as_view(), name='register'),
-    path('add_product/', AddProductView.as_view(), name='add_product'),
-    path('list_products/', product_list, name='list_products' ),
-    path('product_table/', product_table, name='product_table' ),
-    path('update_product/<slug:slug>', UpdateProductView.as_view(), name='update_product'),
-
-    path('product_detail/<slug:slug>', product_detail, name='product_detail'),
-    path('user-profile/', edit_user, name='user-profile' ),
-    path('list_staff/', StaffListView.as_view(), name='list_staff' ),
+    path('', accounts, name='accounts'),
 
 ]
