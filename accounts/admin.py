77cf1d5cfa104155d46bea4f8a235bf05b7d81ec
OCHE Samson <s.oche@outlook.com>
2026-03-04 13:12:39 +0100

    Accounts Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import UserProfile
+from django.contrib.auth.models import User
 
 # Register your models here.
+admin.site.register(UserProfile)
+
+class ProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
