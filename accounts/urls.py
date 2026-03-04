4c0e876950cdd417ab41fdd0ec05c54a499f6b7a
OCHE Samson <s.oche@outlook.com>
2026-03-04 23:14:07 +0100

    accounts sleek3 logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import accounts, edit_user, StaffListView
+from .views import accounts, edit_user, StaffListView, logout_user
 
 
 urlpatterns = [
     path('', accounts, name='accounts'),
+
+    path("logout/", logout_user, name="logout"),
 
     path('user-profile/', edit_user, name='user-profile' ),
     path('list_staff/', StaffListView.as_view(), name='list_staff' ),
