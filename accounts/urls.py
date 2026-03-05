57a2e15f1f1d10acac7e652bba39c903ef5e5d4d
OCHE Samson <s.oche@outlook.com>
2026-03-05 13:15:02 +0100

    accounts admin

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import accounts, edit_user, StaffListView, logout_user
+from .views import accounts, edit_user, StaffListView, logout_user, CustomLoginView
 
 
 urlpatterns = [
     path('', accounts, name='accounts'),
 
+    path('login/', CustomLoginView.as_view, name='login'),
     path("logout/", logout_user, name="logout"),
 
     path('user-profile/', edit_user, name='user-profile' ),
