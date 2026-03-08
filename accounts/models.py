1ccc012a47e5ade2175833bb05a55613c6eaccf3
OCHE Samson <s.oche@outlook.com>
2026-03-08 22:41:26 +0100

    profile1

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     date_modified = models.DateTimeField(User, auto_now=True)
     phone = models.CharField(max_length=20, blank=True, )
-    pix = models.ImageField(upload_to="staff/", blank=True)
+    pix = models.ImageField(upload_to="staff/", blank=True, null=True)
 
     def __str__(self):
         return self.user.username
