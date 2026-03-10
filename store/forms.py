4207038273dea654ba5f45a143c9e63297504a43
OCHE Samson <s.oche@outlook.com>
2026-03-10 03:17:10 +0100

    contact us remove message

--- store/forms.py
+++ store/forms.py
@@ -6,10 +6,10 @@ class ContactUsForm(forms.ModelForm):
 	name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name'}), required=True)
 	phone = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}), required=False)
 	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), required=True)
-	message = forms.CharField(label="", widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Write a message*'}), required=False)
+	message = forms.CharField(label="", widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Type your Message here'}), required=False)
 
-	service = forms.ChoiceField(label="", widget=forms.CharField(attrs={'class':'form-control', 'placeholder':'Select Service',}), required=False)
+	# service = forms.ChoiceField(label="", widget=forms.CharField(attrs={'class':'form-control', 'placeholder':'Select Service',}), required=False)
 
 	class Meta:
 		model = ContactUs
-		fields = ('name', 'phone', 'email', 'message', 'service')
+		fields = ('name', 'phone', 'email', 'message', )
