776b16e3a41aa6a7239b78b6dd3a30ed9314da0f
OCHE Samson <s.oche@outlook.com>
2026-03-10 21:29:04 +0100

    contact page

--- store/forms.py
+++ store/forms.py
@@ -3,13 +3,29 @@ from django import forms
 from .models import ContactUs
 
 class ContactUsForm(forms.ModelForm):
-	name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name'}), required=True)
-	phone = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}), required=False)
-	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), required=True)
-	message = forms.CharField(label="", widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Type your Message here'}), required=False)
-
-	service = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Name'}), required=True)
 
 	class Meta:
 		model = ContactUs
 		fields = ('name', 'phone', 'email', 'message', 'service' )
+
+	def __init__(self, *args, **kwargs):
+		super(ContactUsForm, self).__init__(*args, **kwargs)
+		self.fields["name"].widget.attrs['class'] = 'form-control form-input-field'
+		self.fields["name"].widget.attrs['placeholder'] = 'Name'
+		self.fields["name"].label = ''
+
+		self.fields["phone"].widget.attrs['class'] = 'form-control form-input-field'
+		self.fields["phone"].widget.attrs['placeholder'] = 'Phone'
+		self.fields["phone"].label = ''
+
+		self.fields["email"].widget.attrs['class'] = 'form-control form-input-field'
+		self.fields["email"].widget.attrs['placeholder'] = 'Email'
+		self.fields["email"].label = ''
+
+		self.fields["message"].widget.attrs['class'] = 'form-control form-input-field'
+		self.fields["message"].widget.attrs['placeholder'] = 'Type your Message here'
+		self.fields["message"].label = ''
+
+		self.fields["service"].widget.attrs['class'] = 'form-control form-input-field custom-select my-1 mr-sm-2'
+		self.fields["service"].label = ''
+		self.fields["service"].empty_label = '-------  Select a Service  -------'
