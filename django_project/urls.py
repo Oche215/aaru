5e50a228054b63d8f99586f05dfc2845cb97631a
OCHE Samson <s.oche@outlook.com>
2026-03-13 11:55:19 +0100

    media server

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")), #registartion
 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
