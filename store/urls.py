f57cef40f30f2038ec5cda0b86e515929f9f75b2
OCHE Samson <s.oche@outlook.com>
2026-03-14 20:53:34 +0100

    index

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, details, services, about, contact
+from .views import home, details, services, about, contact, get_product_image, index
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,5 +8,8 @@ urlpatterns = [
 
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+
+    path('index/', index, name='index'),
+    path('product/<int:product_id>/image/', get_product_image, name='product-image')
 
 ]
--- store/views.py
+++ store/views.py
@@ -5,6 +5,9 @@ from .forms import ContactUsForm
 from django.core.mail import send_mail
 from django_project.settings import DEFAULT_FROM_EMAIL
 
+from django.http import HttpResponse
+from django.views.decorators.http import condition
+
 # Create your views here.
 
 def home(request):
@@ -79,3 +82,23 @@ def add_product(request):
         Product.objects.create(name=name, added_by=request.user)
         return redirect('success_page')
     return render(request, 'create_item.html')
+
+
+def get_product_image(request, product_id):
+    """Serve product image from database"""
+    try:
+        product = Product.objects.get(id=product_id)
+        if product.pix:
+            return HttpResponse(product.pix.read(), content_type='image/jpeg')
+    except Product.DoesNotExist:
+        pass
+
+    # Return placeholder if no image
+    return HttpResponse(status=404)
+
+
+def index(request):
+    products = Product.objects.all()
+
+    return render(request, 'store/index.html', {'products': products, })
+
